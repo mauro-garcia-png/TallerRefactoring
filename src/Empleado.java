980f77e97fd68e607772114a8d433bd62e177b26
Chrisxho2006 <chriolme@espol.edu.ec>
2025-08-13 16:45:13 -0500

    metodo imprimir en empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -6,7 +6,9 @@ public abstract class Empleado {
     private double tarifaHora;
     private String genero;
 
-    public Empleado(){}
+    public Empleado() {
+    }
+
     public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento,
             String genero) {
         this.nombre = nombre;
@@ -16,35 +18,43 @@ public abstract class Empleado {
         this.departamento = departamento;
         this.genero = genero;
     }
-        protected void validarDatos(){
-        if(salarioBase <= 0){
+
+    protected void validarDatos() {
+        if (salarioBase <= 0) {
             throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
         }
 
-        if (horasTrabajadas < 0){
+        if (horasTrabajadas < 0) {
             throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
         }
     }
 
-    protected double calcularHorasExtra(){
+    protected double calcularHorasExtra() {
         if (horasTrabajadas > 40) {
-        return (horasTrabajadas - 40) * 50;
+            return (horasTrabajadas - 40) * 50;
         }
         return 0;
 
     }
 
     protected double calcularBonoDepartamento() {
-    switch (departamento) {
-        case "Sistemas":
-            return 20;
-        case "Contabilidad":
-            return 10;
-        default:
-            return 0;
+        switch (departamento) {
+            case "Sistemas":
+                return 20;
+            case "Contabilidad":
+                return 10;
+            default:
+                return 0;
         }
     }
 
+    public void imprimirDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+        System.out.println("Salario: " + salarioBase);
+        System.out.println("Horas trabajadas: " + horasTrabajadas);
+        System.out.println("Departamento: " + departamento);
+    }
 
     public abstract double calcularSalario();
 
